7990547414883190716d7443aebd1f005ecc0554
karthike-05 <karthike@email.unc.edu>
2023-04-28 06:28:00 -0400

    firstfewapi

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ import express from 'express';
 const arg = minimist(process.argv.slice(2))
 const app = express();
 const port = arg.port || 5000 //Set default port to 5000
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app', (req, res) => {
+    res.status(200).send("200 OK"); 
+});
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(rps()); 
+});
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(rpsls()); 
+});
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200).send(rps(req.query.shot)); 
+});
+
+
+
+app.listen(port);
